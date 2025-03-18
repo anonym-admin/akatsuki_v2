445e295ccbfa33e1c0e431b4f91c5c1f13e78517
MK <skdfw1212@naver.com>
2025-03-18 21:40:07 +0900

    aa

---
+++ common/MeshData.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include "Vertex.h"
+
+struct MeshData_t
+{
+	//wchar_t wcMeshName[_MAX_PATH] = {};
+	//wchar_t wcMaterialName[_MAX_PATH] = {};
+
+	Vertex_t* pVertices = nullptr;
+	SkinnedVertex_t* pSkinnedVertices = nullptr;
+	AkU32* pIndices = nullptr;
+	AkU32 uVerticeNum = 0;
+	AkU32 uIndicesNum = 0;
+
+	wchar_t wcAlbedoTextureFilename[_MAX_PATH] = {};
+	wchar_t wcEmissiveTextureFilename[_MAX_PATH] = {};
+	wchar_t wcHeightTextureFilename[_MAX_PATH] = {};
+	wchar_t wcNormalTextureFilename[_MAX_PATH] = {};
+	wchar_t wcMetallicTextureFilename[_MAX_PATH] = {};
+	wchar_t wcRoughnessTextureFilename[_MAX_PATH] = {};
+	wchar_t wcAoTextureFilename[_MAX_PATH] = {};
+	wchar_t wcOpacityTextureFilename[_MAX_PATH] = {};
+};
+
+struct VertexWeight_t
+{
+	AkF32 fBlendWeights[8] = {};
+	AkU8 uBoneIndices[8] = {};
+	AkU8 uWeightNum = 0;
+	AkU8 uIndiceNum = 0;
+};
+
+struct BoneData_t
+{
+	wchar_t wcName[_MAX_PATH] = {};
+	AkI32 iIndex = -1;
+	Matrix mOffset = Matrix();
+};
+
+struct MaterialData_t
+{
+	wchar_t wcName[_MAX_PATH] = {};
+	Vector4 vAmbient = Vector4(0.0f);
+	Vector4 vDiffuse = Vector4(0.0f);
+	Vector4 vSpecular = Vector4(0.0f);
+	Vector4 vEmissive = Vector4(0.0f);
+
+	wchar_t wcAlbedoMapName[_MAX_PATH] = {};
+	wchar_t wcEmissiveMapName[_MAX_PATH] = {};
+	wchar_t wcHeightMapName[_MAX_PATH] = {};
+	wchar_t wcNormalMapName[_MAX_PATH] = {};
+	wchar_t wcMetallicMapName[_MAX_PATH] = {};
+	wchar_t wcRoughnessMapName[_MAX_PATH] = {};
+	wchar_t wcAoMapName[_MAX_PATH] = {};
+	wchar_t wcOpacityMapName[_MAX_PATH] = {};
+};
+
+struct New_MeshData_t
+{
+	wchar_t wcMeshName[_MAX_PATH] = {};
+	wchar_t wcMaterialName[_MAX_PATH] = {};
+
+	Vertex_t* pVertices = nullptr;
+	SkinnedVertex_t* pSkinnedVertices = nullptr;
+	AkU32* pIndices = nullptr;
+	AkU32 uVerticeNum = 0;
+	AkU32 uIndicesNum = 0;
+
+	wchar_t wcAlbedoTextureFilename[_MAX_PATH] = {};
+	wchar_t wcEmissiveTextureFilename[_MAX_PATH] = {};
+	wchar_t wcHeightTextureFilename[_MAX_PATH] = {};
+	wchar_t wcNormalTextureFilename[_MAX_PATH] = {};
+	wchar_t wcMetallicTextureFilename[_MAX_PATH] = {};
+	wchar_t wcRoughnessTextureFilename[_MAX_PATH] = {};
+	wchar_t wcAoTextureFilename[_MAX_PATH] = {};
+	wchar_t wcOpacityTextureFilename[_MAX_PATH] = {};
+};
+
+struct NodeData_t
+{
+	AkI32 iIndex = -1;
+	wchar_t wcName[_MAX_PATH] = {};
+	AkI32 iParent = -1;
+	Matrix mTransform = Matrix();
+};
+
+struct BillboardData_t
+{
+	VertexSize_t* pVertice = nullptr;
+	AkU32 uPointsNum = 0;
+
+	wchar_t wcAlbedoTextureFilename[_MAX_PATH] = {};
+	wchar_t wcEmissiveTextureFilename[_MAX_PATH] = {};
+	wchar_t wcHeightTextureFilename[_MAX_PATH] = {};
+	wchar_t wcNormalTextureFilename[_MAX_PATH] = {};
+	wchar_t wcMetallicTextureFilename[_MAX_PATH] = {};
+	wchar_t wcRoughnessTextureFilename[_MAX_PATH] = {};
+	wchar_t wcAoTextureFilename[_MAX_PATH] = {};
+	wchar_t wcOpacityTextureFilename[_MAX_PATH] = {};
+	wchar_t wcArrayFilename[_MAX_PATH] = {};
+};
+
+struct LineData_t
+{
+	VertexColor_t* pVertices = nullptr;
+	AkU32* pIndices = nullptr;
+	AkU32 uVerticeNum = 0;
+	AkU32 uIndicesNum = 0;
+};
+
---
+++ common/Vertex.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "CommonType.h"
+#include <directxtk/SimpleMath.h>
+#include <directxmesh.h>
+
+using DirectX::SimpleMath::Quaternion;
+using DirectX::SimpleMath::Matrix;
+using DirectX::SimpleMath::Vector4;
+using DirectX::SimpleMath::Vector3;
+using DirectX::SimpleMath::Vector2;
+
+struct Vertex_t
+{
+	Vector3 vPosition = Vector3(0.0f);
+	Vector3 vNormalModel = Vector3(0.0f);
+	Vector2 vTexCoord = Vector2(0.0f);
+	Vector3 vTangentModel = Vector3(0.0f);
+};
+
+struct SkinnedVertex_t
+{
+	Vector3 vPosition = Vector3(0.0f);
+	Vector3 vNormalModel = Vector3(0.0f);
+	Vector2 vTexCoord = Vector2(0.0f);
+	Vector3 vTangentModel = Vector3(0.0f);
+	AkF32 fBlendWeights[8] = {};
+	AkU8 uBoneIndices[8] = {};
+};
+
+struct VertexColorTexcoord_t
+{
+	Vector3 vPosition = Vector3(0.0f);
+	Vector4 vColors = Vector4(0.0f);
+	Vector2 vTexCoord = Vector2(0.0f);
+};
+
+struct VertexTexcoord_t
+{
+	Vector3 vPosition = Vector3(0.0f);
+	Vector2 vTexCoord = Vector2(0.0f);
+};
+
+struct VertexColor_t
+{
+	Vector3 vPosition = Vector3(0.0f);
+	Vector3 vColor = Vector3(0.0f);
+};
+
+struct VertexSize_t
+{
+	Vector4 vPosition = Vector4(0.0f);
+	Vector2 vSize = Vector2(1.0f);
+	Vector2 vPadding;
+};
+
+struct VertexNormalTexcoordTangentAlpha_t
+{
+	Vector3 vPosition = Vector3(0.0f);
+	Vector3 vNormalModel = Vector3(0.0f);
+	Vector2 vTexCoord = Vector2(0.0f);
+	Vector3 vTangentModel = Vector3(0.0f);
+	AkF32 pAlpha[4] = {};
+};
+
+struct VertexParticle_t
+{
+	Vector4 vPosition = Vector4(0.0f);
+	Vector4 vColor = Vector4(1.0f);
+	AkF32 fStartLifeTime = 1.0f;
+	AkF32 fSpeed = 1.0f;
+	Vector2 vSize = Vector2(1.0f);
+	Vector3 vDirection = Vector3(0.0f);
+	AkF32 fPadding = 0.0f;
+};
+
