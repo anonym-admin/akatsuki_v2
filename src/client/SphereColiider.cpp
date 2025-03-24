7c94e862e0a910fe0f0e364722240d9e1080569d
MK <skdfw1212@naver.com>
2025-03-24 09:01:04 +0900

    Update sphere collider.

--- src/client/SphereColiider.cpp
+++ src/client/SphereColiider.cpp
@@ -2,6 +2,11 @@
 #include "SphereColiider.h"
 #include "Transform.h"
 
+static bool useSnap(false);
+static float snap[3] = { 1.f, 1.f, 1.f };
+static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
+static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::WORLD);
+
 /*
 ====================
 Sphere Collider
@@ -76,6 +81,110 @@ void SphereCollider::OnCollisionExit(Collider* pCollider)
 
 void SphereCollider::RenderGUI()
 {
+	std::wstring ModelName = L"Sphere Collider";
+	char Title[_MAX_PATH] = {};
+	strcpy_s(Title, ToString(ModelName + L" gizmo").c_str());
+
+	Matrix& mWorldRow = _pTransform->GetWorldTransform();
+
+	ImGuizmo::BeginFrame();
+	ImGui::Begin(Title);
+	ImGui::Checkbox("Use Gizmo", &_bUseGizmo);
+
+	if (!_bUseGizmo)
+	{
+		ImGui::End();
+		return;
+	}
+
+	if (ImGuizmo::IsUsing())
+	{
+		ImGui::Text("Using gizmo");
+	}
+	else
+	{
+		ImGui::Text(ImGuizmo::IsOver() ? "Over gizmo" : "");
+		ImGui::SameLine();
+		ImGui::Text(ImGuizmo::IsOver(ImGuizmo::TRANSLATE) ? "Over translate gizmo" : "");
+		ImGui::SameLine();
+		ImGui::Text(ImGuizmo::IsOver(ImGuizmo::ROTATE) ? "Over rotate gizmo" : "");
+		ImGui::SameLine();
+		ImGui::Text(ImGuizmo::IsOver(ImGuizmo::SCALE) ? "Over scale gizmo" : "");
+	}
+
+	if (ImGui::IsKeyPressed(ImGuiKey_Z))
+		mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+	if (ImGui::IsKeyPressed(ImGuiKey_X))
+		mCurrentGizmoOperation = ImGuizmo::ROTATE;
+	if (ImGui::IsKeyPressed(ImGuiKey_C)) // r Key
+		mCurrentGizmoOperation = ImGuizmo::SCALE;
+
+	if (ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
+		mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+	ImGui::SameLine();
+	if (ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
+		mCurrentGizmoOperation = ImGuizmo::ROTATE;
+	ImGui::SameLine();
+	if (ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
+		mCurrentGizmoOperation = ImGuizmo::SCALE;
+
+	float matrixTranslation[3], matrixRotation[3], matrixScale[3];
+	ImGuizmo::DecomposeMatrixToComponents((float*)&mWorldRow._11, matrixTranslation, matrixRotation, matrixScale);
+	ImGui::InputFloat3("Tr", matrixTranslation);
+	ImGui::InputFloat3("Rt", matrixRotation);
+	ImGui::InputFloat3("Sc", matrixScale);
+	ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, (float*)&mWorldRow._11);
+
+	if (mCurrentGizmoOperation != ImGuizmo::SCALE)
+	{
+		if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
+			mCurrentGizmoMode = ImGuizmo::LOCAL;
+		ImGui::SameLine();
+		if (ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
+			mCurrentGizmoMode = ImGuizmo::WORLD;
+	}
+
+	if (ImGui::IsKeyPressed(ImGuiKey_V))
+		useSnap = !useSnap;
+	ImGui::Checkbox(" ", &useSnap);
+	ImGui::SameLine();
+	switch (mCurrentGizmoOperation)
+	{
+	case ImGuizmo::TRANSLATE:
+		ImGui::InputFloat3("Snap", &snap[0]);
+		break;
+	case ImGuizmo::ROTATE:
+		ImGui::InputFloat("Angle Snap", &snap[0]);
+		break;
+	case ImGuizmo::SCALE:
+		ImGui::InputFloat("Scale Snap", &snap[0]);
+		break;
+	}
+
+	float windowWidth = (float)ImGui::GetWindowWidth();
+	float windowHeight = (float)ImGui::GetWindowHeight();
+
+	ImGuiIO& io = ImGui::GetIO();
+	ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+
+	Matrix mView = Matrix();
+	Matrix mProj = Matrix();
+	GRenderer->GetViewPorjMatrix(&mView, &mProj);
+	mView = mView.Transpose();
+	mProj = mProj.Transpose();
+
+	ImGuizmo::Manipulate((float*)&mView, (float*)&mProj, mCurrentGizmoOperation, mCurrentGizmoMode, (float*)&mWorldRow._11, NULL, useSnap ? &snap[0] : NULL);
+
+	Vector3 vScale = Vector3(1.0f);
+	Quaternion qRotation = Quaternion();
+	Vector3 vPosition = Vector3(0.0f);
+
+	mWorldRow.Decompose(vScale, qRotation, vPosition);
+
+	_pTransform->SetScale(&vScale);
+	_pTransform->SetPosition(&vPosition);
+
+	ImGui::End();
 }
 
 AkF32 SphereCollider::Radius()
